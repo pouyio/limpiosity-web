8049219fda208736e988431de1aa0fd0f68a8af1
Vicente <pouyio@gmail.com>
2016-03-10 22:28:02 +0100

    first version panel-6

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+(function initialize() {
+  var albacete = new google.maps.LatLng(38.9942400, -1.8564300)
+  var mapProp = {
+    center: albacete,
+    zoom: 5,
+    disableDefaultUI:true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var marker = new google.maps.Marker({
+    position: albacete,
+    title: "Albacete"
+  });
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  marker.setMap(map);
+})();
