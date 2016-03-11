565d790ab540899b7360e5fac806c699f1243bfc
Vicente <pouyio@gmail.com>
2016-03-11 13:46:30 +0100

    form animations added in js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,6 @@
 (function initialize() {
+
+  // MAP
   var albacete = new google.maps.LatLng(38.9942400, -1.8564300)
   var mapProp = {
     center: albacete,
@@ -12,4 +14,29 @@
   });
   var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
   marker.setMap(map);
+
+
 })();
+// CONTACT FORM
+function onFocus(element) {
+  var lb = element.getAttribute("name");
+  var label = document.getElementById(lb);
+  label.style.top = "-1em";
+  label.style["font-size"] = ".8em";
+  label.style.color = "#78bdf2";
+}
+
+function focusLost(element) {
+  var lb = element.getAttribute("name");
+  var label = document.getElementById(lb);
+  console.log(element.value);
+  if(element.value == "") {
+    if(label == "message") {
+      label.style.top = "3em";
+    } else {
+      label.style.top = "1em";
+    }
+    label.style["font-size"] = "initial";
+    label.style.color = "initial";
+  }
+}
