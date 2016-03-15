8f9f205663d429de75f52db3f6f96b6228a1a726
Vicente <pouyio@gmail.com>
2016-03-15 20:15:09 +0100

    half navbar responsive coded

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,13 @@
 (function initialize() {
+  // NAVBAR
+
 
   // MAP
   var albacete = new google.maps.LatLng(38.9942400, -1.8564300)
   var mapProp = {
     center: albacete,
     zoom: 5,
-    disableDefaultUI:true,
+    disableDefaultUI: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var marker = new google.maps.Marker({
@@ -17,12 +19,11 @@
 
   // SLIDESHOW
   var opts = {
-    auto : false,
-    fullScreen : false,
-    swipe : true
-};
-console.log('this is workings');
-makeBSS('.bss-slides', opts);
+    auto: false,
+    fullScreen: false,
+    swipe: true
+  };
+  makeBSS('.bss-slides', opts);
 })();
 // CONTACT FORM
 function onFocus(element) {
@@ -36,8 +37,8 @@ function onFocus(element) {
 function focusLost(element) {
   var lb = element.getAttribute("name");
   var label = document.getElementById(lb);
-  if(element.value == "") {
-    if(label == "message") {
+  if (element.value == "") {
+    if (label == "message") {
       label.style.top = "3em";
     } else {
       label.style.top = "1em";
