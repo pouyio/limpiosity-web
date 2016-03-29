6459f79c6d7601507329782d255b8bf2f54aaa96
Vicente <pouyio@gmail.com>
2016-03-29 14:07:28 +0200

    markdown loading script created

---
+++ app/scripts/load-content.js
@@ -0,0 +1,14 @@
+// Markdown parser for JS https://github.com/chjj/marked
+function load(file) {
+  var mdFile = new XMLHttpRequest();
+  mdFile.open("GET", "/scripts/" + file + ".md", true);
+  mdFile.send();
+  mdFile.onreadystatechange = function() {
+    // Makes sure the document exists and is ready to parse.
+    if (mdFile.readyState === 4 && mdFile.status === 200) {
+      var mdText = mdFile.responseText;
+      document.getElementById('md-content').innerHTML =
+      marked(mdText);
+    }
+  }
+}
